00c59ba9b488a937c3aacfebdb976807d0193c01
Teeco123 <kacper.gajko1@gmail.com>
2024-09-09 17:06:36 +0200

    Formatted with RustFmt

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,24 @@ use eframe::egui;
 
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("Pixliz", native_options, Box::new(|cc| Ok(Box::new(MyApp::new(cc)))))
+    eframe::run_native(
+        "Pixliz",
+        native_options,
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
+    )
 }
 
 #[derive(Default)]
-struct MyApp{}
+struct MyApp {}
 
 impl MyApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self{
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self::default()
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui|{
-            
-        });
+        egui::CentralPanel::default().show(ctx, |ui| {});
     }
 }
