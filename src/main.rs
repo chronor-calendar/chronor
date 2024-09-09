964aa46fdf451ff66658d48975c60c90b97e1df7
Teeco123 <kacper.gajko1@gmail.com>
2024-09-09 17:33:26 +0200

    Added min size & title options

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@
 use eframe::egui;
 
 fn main() -> eframe::Result {
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_title("Pixliz")
+            .with_min_inner_size([330.0, 220.0]),
+        ..Default::default()
+    };
     eframe::run_native(
         "Pixliz",
         native_options,
