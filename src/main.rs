df639d47731bbe7dc0ad055b555b69f26eae2aad
Teeco123 <kacper.gajko1@gmail.com>
2024-09-10 02:21:05 +0200

    Added app icon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use eframe::egui;
 
@@ -7,9 +6,14 @@ fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_title("Pixliz")
-            .with_min_inner_size([330.0, 220.0]),
+            .with_min_inner_size([330.0, 220.0])
+            .with_icon(
+                eframe::icon_data::from_png_bytes(&include_bytes!("../images/icon.png")[..])
+                    .expect("Failed to load icon"),
+            ),
         ..Default::default()
     };
+
     eframe::run_native(
         "Pixliz",
         native_options,
@@ -34,14 +38,6 @@ impl eframe::App for MyApp {
                 ui.menu_button("Edit", |ui| {});
                 ui.menu_button("Window", |ui| {});
             });
-        });
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            let button = egui::Button::new("Button")
-                .fill(egui::Color32::from_rgb(128, 128, 128))
-                .rounding(0.0);
-
-            ui.add(button)
         });
     }
 }
